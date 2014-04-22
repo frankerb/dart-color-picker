dadc82efbb3cb67ca21d947b13380756471baaa5
frankerb <frankerb@users.noreply.github.com>
2014-04-22 10:26:48 +0200

    Didn't work in newer versions of Dart

--- lib/widgets/hsv_gradient_picker.dart
+++ lib/widgets/hsv_gradient_picker.dart
@@ -58,7 +58,7 @@ class HsvGradientPicker implements HueChangeListener {
   HsvGradientPicker(int width, int height, [this._color]) {
     canvas = new CanvasElement(width: width, height: height);
     canvas.classes.add("color-picker-hsv-gradient");
-    context = canvas.context2D;
+    context = canvas.getContext("2d");
     if (_color == null) {
       _color = new ColorValue();
     }
